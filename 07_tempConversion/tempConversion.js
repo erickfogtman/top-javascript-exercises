9524fe94f48c7a8f39802ac14bae8db57b55b410
Erick Fogtman <erickfogtman@gmail.com>
2023-09-22 14:30:06 -0300

    Add solution for 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+// write a function that converts fahrenheit to celsius, with decimals rounded to 1 place
+const convertToCelsius = function (temp) {
+  return Math.round((temp - 32) * 5 / 9 * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+// write a function that conerts celsius to fahrenheit, with decimals rounded to 1 place
+const convertToFahrenheit = function (temp) {
+  return Math.round((temp * 9 / 5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
