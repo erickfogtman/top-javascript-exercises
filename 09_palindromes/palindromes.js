084bb369ce88c86ee7bbad4fef03663857cd7d40
Erick Fogtman <erickfogtman@gmail.com>
2023-09-25 15:10:07 -0300

    Add solution for 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
-
-};
+const palindromes = function (string) {
+    const regex = /[^a-z0-9]/g;
+    const lowerCaseString = string.toLowerCase().replace(regex, "");
+    const reversedString = lowerCaseString.split("").reverse().join("");
+    return lowerCaseString === reversedString;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
