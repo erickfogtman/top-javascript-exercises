509bfb2b62c5e6a4dafeac7db838ba713d80d3df
Erick Fogtman <erickfogtman@gmail.com>
2023-09-25 15:14:29 -0300

    Add solution for 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+//You are given an array of objects that represent books with an author and a title. Return a new array containing only the titles of the books.
+const getTheTitles = function (arr) {
+    let titles = [];
+    arr.forEach((book) => titles.push(book.title));
+    return titles;
 };
 
 // Do not edit below this line
