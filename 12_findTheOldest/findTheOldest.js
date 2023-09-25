67e3fc31184f9b9c1d114e793373ac00e896fe5b
Erick Fogtman <erickfogtman@gmail.com>
2023-09-25 15:17:04 -0300

    Add solution to 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+// Find the oldest person in the array of objects
+const findTheOldest = function (arr) {
+    let oldestPerson = arr[0];
+    arr.forEach((person) => {
+        if (getAge(person) > getAge(oldestPerson)) {
+            oldestPerson = person;
+        }
+    });
+    return oldestPerson;
+};
 
+const getAge = function (person) {
+    let death = person.yearOfDeath;
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - person.yearOfBirth;
 };
 
 // Do not edit below this line
