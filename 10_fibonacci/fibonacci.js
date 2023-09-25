a9ce41d41b78e8e28191a8f7ad5372af89b540dc
Erick Fogtman <erickfogtman@gmail.com>
2023-09-25 15:12:00 -0300

    Add solution for 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+// Write a function that returns the nth number in the Fibonacci sequence.
+const fibonacci = function (num) {
+    if (num < 0) return "OOPS";
+    if (num === 0) return 0;
+    if (num === 1) return 1;
+    let fib = [0, 1];
+    for (let i = 2; i <= num; i++) {
+        fib[i] = fib[i - 2] + fib[i - 1];
+    }
+    return fib[num];
 };
 
 // Do not edit below this line
